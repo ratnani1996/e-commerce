9108b3f5e0ed3cd060f08834f5490240fab2c6bf
ratnani1996 <himanshuratnani1996@gmail.com>
2018-02-04 14:50:06 +0530

    added function to connect to database

--- server.js
+++ server.js
@@ -3,12 +3,8 @@ const express = require('express')
 const app = express();
 
 //connect to mongodatabase
-const mongoose = require('mongoose')
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URL)
-mongoose.connection
-        .once('connected', ()=>{console.log(`Connection to the database up and running`)})
-        .on('error', (err)=>{console.log(err)})
+var {connectToDatabase} = require('./config/database');
+connectToDatabase();
 
 
 
